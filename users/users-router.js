ada673f1b020cc178795ac63d6fbed61eb5bf717
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-06-10 23:57:23 +0800

    built auth router

--- users/users-router.js
+++ users/users-router.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const db = require("./users-model");
+const Users = require("./users-model");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  db.find()
+  Users.find()
     .then(users => {
       res.status(200).json(users);
     })
